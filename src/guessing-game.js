6bbf0e856eceed2f4d61fb516003aa230e0aeaff
tatyana465 <tanyaesti7@gmail.com>
2021-09-03 22:22:18 +0300

    feat:done

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,28 @@
 class GuessingGame {
-    constructor() {}
+    
+    constructor() {
+
+    }
 
     setRange(min, max) {
-
+      this.minMax=[min,max];
+      this.range=max-min;
     }
 
     guess() {
-
+      this.number=Math.ceil(this.minMax[0]+this.range/2);  
+      return this.number;
     }
 
     lower() {
-
+      this.minMax[1]=this.number;
+      this.range=this.minMax[1]-this.minMax[0];
+       
     }
 
     greater() {
-
+      this.minMax[0]=this.number;
+      this.range=this.minMax[1]-this.minMax[0];
     }
 }
 
